46205b0aa7f5662dcb3d74f1539661131fe678c7
Tim Olson <timjolson@users.noreply.github.com>
2018-09-27 15:45:30 -0500

    moved utils from entryWidget to generalUtils

--- tests/sympy_widgets/test_sympyautocolorlineedit.py
+++ tests/sympy_widgets/test_sympyautocolorlineedit.py
@@ -1,7 +1,7 @@
 import pytest
 from sympyEntryWidget.sympy_widget import *
 from generalUtils.helpers_for_tests import *
-from entryWidget.utils import getCurrentColor
+from generalUtils import getCurrentColor
 from sympy import Symbol
 import logging
 import sys
